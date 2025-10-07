be50889a22247d13c5312bcddb0071456d426e3b
Miguel Angel Gambero <mgamrio@g.educaand.es>
2025-10-07 20:34:08 +0200

    dwes

---
+++ gamberoMiguel(Ejercicios PHP - Relacion II - Formulario)/ejercicio1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    <h1><u>Ejercicio 1</u></h1>
+    <!-- 1- Crea un formulario de entrada para una calculadora en PHP a partir de dos
+    números enteros y un operador. Para la introducción de datos, utilizaremos dos
+    campos de texto y un select que contenga como opción diferentes operadores :
+    +,-,*,/,%. Probaremos el envío mediante los métodos GET y POST y
+    apreciaremos las diferencias.(El php en el mismo documento, lo mas sencillo posible y facil de leer)Que el resultado se vea cuando le des a calcular
+    -->
+    
+</body>
+</html>
